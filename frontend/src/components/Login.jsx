e41c5fe9434cb99926e9e39ec2875978ad4a5819
Vishal Prajapati <vishal.2428cseaiml16@kiet.edu>
2026-04-23 16:45:40 +0530

    last commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,79 +1,33 @@
-import axios from "axios";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import axios from 'axios';
 const API = import.meta.env.VITE_API_URL;
+import {useState} from 'react';
+import {useNavigate,Link} from 'react-router-dom';
 
 export default function Login(){
+const nav=useNavigate();
+const [data,setData]=useState({email:'',password:''});
 
-const [data,setData]=useState({
- email:"",
- password:""
-});
-
-const login = async (e) => {
- e.preventDefault();
-
- try{
-
-  const res = await axios.post(
-   `${API}/api/login`,
-   data
-  );
-
-  console.log(res.data);
-
-  localStorage.setItem("token",res.data.token);
-  localStorage.setItem("studentName",res.data.name || "Student");
-  localStorage.setItem("studentEmail",data.email);
-
-  window.location.href="/dashboard";
-
- }catch(err){
-   console.error(err);
-   alert("Login failed");
- }
-
-};
+const login=async(e)=>{
+e.preventDefault();
+const res = await axios.post(
+`${API}/api/login`,
+data
+)
+localStorage.setItem('token',res.data.token)
+localStorage.setItem('studentName',res.data.name || 'Student')
+localStorage.setItem('studentEmail',data.email)
+window.location='/dashboard';
+}
 
 return(
-<div className="auth-container">
-
-<form className="auth-card" onSubmit={login}>
-
+<div className='auth-container'>
+<form className='auth-card' onSubmit={login}>
 <h1>Student Grievance Portal</h1>
-
-<p>Login to continue</p>
-
-<input
-className="input"
-placeholder="Email"
-onChange={(e)=>
-setData({...data,email:e.target.value})
-}
-/>
-
-<input
-type="password"
-className="input"
-placeholder="Password"
-onChange={(e)=>
-setData({...data,password:e.target.value})
-}
-/>
-
-<button type="submit" className="btn">
-Login
-</button>
-
-<p style={{marginTop:"20px"}}>
-New User?
-<Link to="/register"> Create Account</Link>
-</p>
-
+<p>Login to manage your complaints</p>
+<input className='input' placeholder='Email' onChange={e=>setData({...data,email:e.target.value})}/>
+<input className='input' type='password' placeholder='Password' onChange={e=>setData({...data,password:e.target.value})}/>
+<button className='btn'>Login</button>
+<p style={{marginTop:20}}>New User? <Link to='/register'>Create Account</Link></p>
 </form>
-
 </div>
-)
-
-}
+)}
