574b7a269c62fe8a476f744f32919f155ad03036
Vishal Prajapati <vishal.2428cseaiml16@kiet.edu>
2026-04-23 15:06:59 +0530

    health check

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req,res)=>{
+ res.send('API Running');
+});
+
 app.use('/api', require('./routes/authRoutes'));
 app.use('/api/grievances', require('./routes/grievanceRoutes'));
 
